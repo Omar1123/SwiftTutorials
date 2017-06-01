af7260b191f55d8d1f972768f5d995c8d24f4928
Omar <oveliz@plusti.com>
2017-06-01 08:53:39 -0600

    ADD: Miracle app

---
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Omar on 6/1/17.
+//  Copyright © 2017 PlustiPlusti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
