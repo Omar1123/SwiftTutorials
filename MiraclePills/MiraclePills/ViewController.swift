e2280fe3e0d031dbc6eaa0489a151ffa8439b318
Omar <oveliz@plusti.com>
2017-06-07 11:59:35 -0600

    ADD: Picker and new fields

--- MiraclePills/MiraclePills/ViewController.swift
+++ MiraclePills/MiraclePills/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var statePicker: UIPickerView!
+    @IBOutlet var stateBtnPicker: UIButton!
+    
+    let states = ["Alaska", "Arkansas","Alabama","California","Maine","New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         //self.view.backgroundColor = UIColor.purple
     }
 
@@ -19,7 +26,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateBtnPicker.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    
+    @IBAction func btnStatePressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
 }
 
